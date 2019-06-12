78c4a18bb4c1932b849645d1be8422bdb553b9d2
Corne Plas <vdplas@gmail.com>
2019-06-12 13:01:30 +0000

    First commit

---
+++ src/main/java/com/github/corneplas/App.java
@@ -0,0 +1,15 @@
+package com.github.corneplas;
+
+import io.javalin.Javalin;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+      Javalin app = Javalin.create().start(7000);
+      app.get("/", ctx -> ctx.result("hello world!"));
+    }
+}
